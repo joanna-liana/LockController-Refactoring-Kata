9a78e3141737b9813eec4ed149709167493762d8
Joanna Liana <joanna_liana@outlook.com>
2022-11-14 16:32:59 +0100

    Add mock connection

--- typescript/tests/LockController.test.ts
+++ typescript/tests/LockController.test.ts
@@ -1,4 +1,5 @@
 import { IMassiveDbConnection } from '../src/IMassiveDbConnection';
+import { MassiveLocation } from '../src/IMassiveLocation';
 import { MassiveObject } from '../src/IMassiveObject';
 import { LockController } from '../src/LockController';
 
@@ -6,7 +7,13 @@ describe("LockController", () => {
   it("locks and then unlocks", () => {
     // TODO: finish writing this test
     const m = new MassiveObject("Id1");
-    const connection: IMassiveDbConnection = null as any; // TODO
+    const connection: IMassiveDbConnection = {
+      session: {},
+      getPathTo: () => 'TEST PATH',
+      fullObject: () => ({ Id: "1" }),
+      wangle: () => new MassiveLocation(),
+      callMassiveMethod: () => 0
+    };
     const controller = new LockController(connection);
     expect(controller.isSubscriptionActive()).toBeTruthy();
     controller.lockObject(m.Id);
