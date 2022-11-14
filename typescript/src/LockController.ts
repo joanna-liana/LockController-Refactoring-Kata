e8758b0c2001fd87f1f2cb998a7a4d0635548458
Joanna Liana <joanna_liana@outlook.com>
2022-11-14 17:06:49 +0100

    Move subscription setup to factory, making it injectable in controller

--- typescript/src/LockController.ts
+++ typescript/src/LockController.ts
@@ -1,35 +1,14 @@
 ﻿import { IMassiveDbConnection } from './IMassiveDbConnection';
 import { IMassiveVariant } from './IMassiveVariant';
-import { MassiveDataChangedEvent } from './MassiveDataChangedEvent';
 import { Subscription } from './Subscription';
 
 export class LockController {
-  private readonly _subscription: Subscription;
   private _lockedItems: Record<string, IMassiveVariant[]> = {};
 
-  constructor(private readonly _connection: IMassiveDbConnection) {
-
-    this._subscription = new Subscription(_connection.session);
-    this._subscription.publishingEnabled = true;
-    this._subscription.publishingInterval = 500;
-    this._subscription.registerCallback(this.subscriptionDataChange.bind(this));
-    this._subscription.create();
-  }
-
-  /**
-   * This is called by the Massive Database when data is changed,
-   * because we registered it as a callback earlier.
-   */
-  private subscriptionDataChange(subscription: Subscription, e: MassiveDataChangedEvent) {
-    const mObjectId = e.mid;
-
-    if (e.type == "Lock") {
-      this.lockObject(mObjectId);
-    }
-    else if (e.type == "Unlock") {
-      this.unlockObject(mObjectId);
-    }
-  }
+  constructor(
+    private readonly _connection: IMassiveDbConnection,
+    private readonly _subscription: Subscription
+  ) {}
 
   lockObject(mObjectId: string) {
     const path = this._connection.getPathTo(mObjectId);
---
+++ typescript/src/LockControllerFactory.ts
@@ -0,0 +1,41 @@
+﻿import { IMassiveDbConnection } from './IMassiveDbConnection';
+import { LockController } from './LockController';
+import { MassiveDataChangedEvent } from './MassiveDataChangedEvent';
+import { Subscription } from './Subscription';
+
+export class LockControllerFactory {
+  private readonly _subscription: Subscription;
+  private readonly _controller: LockController;
+
+  private constructor(_connection: IMassiveDbConnection) {
+    this._subscription = new Subscription(_connection.session);
+    this._subscription.publishingEnabled = true;
+    this._subscription.publishingInterval = 500;
+    this._subscription.create();
+
+    this._controller = new LockController(_connection, this._subscription);
+
+    this._subscription.registerCallback(this.subscriptionDataChange.bind(this));
+  }
+
+  static createWith(_connection: IMassiveDbConnection) {
+    const manager = new LockControllerFactory(_connection);
+
+    return manager._controller;
+  }
+
+  /**
+   * This is called by the Massive Database when data is changed,
+   * because we registered it as a callback earlier.
+   */
+  private subscriptionDataChange(subscription: Subscription, e: MassiveDataChangedEvent) {
+    const mObjectId = e.mid;
+
+    if (e.type == "Lock") {
+      this._controller.lockObject(mObjectId);
+    }
+    else if (e.type == "Unlock") {
+      this._controller.unlockObject(mObjectId);
+    }
+  }
+}
